a719edab34e5fa3dc2281c4bbab710c604caa3e7
ShunHua <j860404j860404@gmail.com>
2019-09-17 17:16:14 +0800

    update

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      todos: [
-        { text: 'one' },
-        { text: 'two' },
-        { text: 'three' }
-      ]
-    },
+  el: '#app',
+  data: {
+    todos: [
+      { text: 'one' },
+      { text: 'two' },
+      { text: 'three' }
+    ]
+  },
   methods: {
-  	toggle: function(todo){
+    toggle: function (todo) {
       console.log("Before", todo.done)
       todo.done = !todo.done
       console.log("After", todo.done)
     }
   }
-  })
+})
